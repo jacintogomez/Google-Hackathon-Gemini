4964b40e3c193774f06185ca713438961904d6ab
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-04-29 00:15:13 -0400

    asynchronous machine response via gemini

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import google.generativeai as genai
+from google.cloud import translate_v2 as translate
+from dotenv import load_dotenv
+import os
+import time
+
+load_dotenv()
+GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
+model=genai.GenerativeModel('gemini-pro')
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
@@ -16,16 +24,31 @@ def process_language():
 def conversation(language):
     return render_template('microphone.html',language=language)
 
-@app.route('/process_intermediate',methods=['POST'])
+@app.route('/process_human',methods=['POST'])
 def process_intermediate():
-    human_response=request.form['human_input'].upper()
-    machine_response=human_response.lower()
-    return {'human_response':human_response,'machine_response':machine_response}
+    human_response=request.form['human_input']
+    return jsonify(human_response=human_response)
 
-@app.route('/process_machine',methods=['GET'])
+@app.route('/process_machine',methods=['POST'])
 def process_machine():
+    machinput=request.form['human_input']
+    machine_response=machine_turn(machinput)
+    return jsonify(machine_response=machine_response)
 
-    return machine_response.lower()
+def generate_response(input_text):
+    prompt='Please respond logically to the following sentence in a conversation: '+input_text
+    generation=model.generate_content(prompt).text
+    #print(generation)
+    return generation
+
+def machine_turn(text):
+    machine_file='recordings/machine.wav'
+    talk=generate_response(text)
+    # make_speech_file(machine_file,talk)
+    # eng=translate_to(english,machine_file)
+    # play_audio(machine_file)
+    # print('Computer: '+talk+' ('+eng+')')
+    return talk
 
 if __name__ == '__main__':
     app.run(debug=True)
